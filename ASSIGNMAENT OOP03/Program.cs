edec586815c17425d6383f553eb599e82a6479cd
yassin991 <yassenelsherbiny69@gmail.com>
2026-02-24 16:31:37 +0200

    Add project files.

---
+++ ASSIGNMAENT OOP03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ASSIGNMAENT_OOP03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
